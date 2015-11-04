c272c7625bdce169c74df62dfad57b9ea5ce2b5f
Jackson <jlee@Jacksons-MacBook-Pro.local>
2015-11-04 11:23:02 -0500

    Finished Registration and Confirmation for users. When Users sign up they get a notification email.

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -5,22 +5,8 @@ class Api::UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+    @user = User.includes(:listings).find(params[:id])
     render json: @user
-  end
-
-  def new
-    @user = User.new
-    render json: @user
-  end
-
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      render json: @user
-    else
-      render json: { errors: @user.errors.full_messages }, status: 422
-    end
   end
 
   def edit
