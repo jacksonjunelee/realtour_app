a8e3b07e1c0ff60327333e733315255bf408ceb3
Jackson <jlee@Jacksons-MacBook-Pro.local>
2015-11-18 17:58:05 -0500

    change deploy  directory to /home/deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ lock '3.4.0'
 set :application, 'realtour_app'
 set :repo_url, 'git@github.com:jjlee5266/realtour_app.git'
 set :branch, :master
-set :deploy_to, '/var/apps/realtour_app'
+set :deploy_to, '/home/deploy/realtour_app'
 set :pty, true
 set :linked_files, %w{config/database.yml config/application.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
