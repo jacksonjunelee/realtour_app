46565eda2e4bdb7166aa6697f099ed8085208ff6
Winson <winsonyuan1@gmail.com>
2016-01-06 16:56:02 -0500

    was kinda productive at first then just stop caring

--- frontend/javascript/footer.js
+++ frontend/javascript/footer.js
@@ -1,5 +1,52 @@
+var footerData = {
+	options: [
+		{
+			id: 1,
+			content: "About Us"
+		},
+		{
+			id: 2,
+			content: "Contact Us"
+		},
+		{
+			id: 3,
+			content: "Blog"
+		},
+		{
+			id: 4,
+			content: "Help"
+		},
+		{
+			id: 5,
+			content: "Privacy"
+		},
+		{
+			id: 6,
+			content: "Terms and Conditions"
+		},
+		{
+			id: 7,
+			content: "facebook"
+		},
+		{
+			id: 8,
+			content: "twitter"
+		},
+		{
+			id: 9,
+			content: "instagram"
+		},
+		{
+			id: 10,
+			content: "pinterest"
+		}
+	]
+}
+
 var FooterSection = React.createClass({
 	render: function () {
+		var options = this.props.footerData.options;
+		
 		return (
 			<ul class="global-footer">
 				<li>Some option1</li>
