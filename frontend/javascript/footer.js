1437e23ea08e14951d5949fae45cfc8e6dc62355
Winson <winsonyuan1@gmail.com>
2016-01-11 17:12:59 -0500

    updated react footer

--- frontend/javascript/footer.js
+++ frontend/javascript/footer.js
@@ -1,68 +1,95 @@
-var footerData = {
-	options: [
-		{
-			id: 1,
-			content: "About Us"
-		},
-		{
-			id: 2,
-			content: "Contact Us"
-		},
-		{
-			id: 3,
-			content: "Blog"
-		},
-		{
-			id: 4,
-			content: "Help"
-		},
-		{
-			id: 5,
-			content: "Privacy"
-		},
-		{
-			id: 6,
-			content: "Terms and Conditions"
-		},
-		{
-			id: 7,
-			content: "facebook"
-		},
-		{
-			id: 8,
-			content: "twitter"
-		},
-		{
-			id: 9,
-			content: "instagram"
-		},
-		{
-			id: 10,
-			content: "pinterest"
+( function () {
+	var footerData = {
+		options: [
+			{
+				id: 1,
+				content: "About Us",
+				link: "#"
+			},
+			{
+				id: 2,
+				content: "Contact Us",
+				link: "#"
+			},
+			{
+				id: 3,
+				content: "Blog",
+				link: "#"
+			},
+			{
+				id: 4,
+				content: "Help",
+				link: "#"
+			},
+			{
+				id: 5,
+				content: "Privacy",
+				link: "#"
+			},
+			{
+				id: 6,
+				content: "Terms and Conditions",
+				link: "#"
+			},
+			{
+				id: 7,
+				content: "facebook",
+				link: "#"
+			},
+			{
+				id: 8,
+				content: "twitter",
+				link: "#"
+			},
+			{
+				id: 9,
+				content: "instagram",
+				link: "#"
+			},
+			{
+				id: 10,
+				content: "pinterest",
+				link: "#"
+			}
+		]
+	};
+
+	var ListCmpt = React.createClass({
+		render: function () {
 		}
-	]
-}
+	})
 
-var FooterSection = React.createClass({
-	render: function () {
-		var options = this.props.footerData.options;
+	var FooterSection = React.createClass({
+		render: function () {
+			//var options = this.props.footerData.options;
+			var options = this.props.data.options;
+			console.log('this is it:', this)
+			return (
+				<div className="global-footer-section clearfix">
+					<ul>
+						{
+							options.map( function ( option ) {
+								var optName = option.content;
 
-		return (
-			<ul className="global-footer">
-				<li className="">
-				{
-					options.map( function ( option ) {
+								return (
+									<li key={ option.id } className="global-footer-section--option-list">
+										<a href={ option.link }>
+											{ optName }
+										</a>
+									</li>
+								);
+							})
+						}
+					</ul>
+				</div>
+			);
+		}
+	});
 
-					});
-				}
-				</li>
-			</ul>
-		);
-	}
-});
+	ReactDOM.render(
+		<FooterSection data={footerData} />,
+		document.getElementById( 'react-footer')
+	);
 
-ReactDOM.render(
-	<FooterSection />,
-	document.getElementById( 'react-footer')
-);
 
+})();
